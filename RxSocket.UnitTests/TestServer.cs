45918612b26fe66c86d616a1426c441376ccaeb5
ricoisme <rico0203@msn.com>
2019-03-01 16:25:21 +0800

    check contect of record

--- RxSocket.UnitTests/TestServer.cs
+++ RxSocket.UnitTests/TestServer.cs
@@ -30,19 +30,17 @@ namespace RxSocket.UnitTests
             _mocks.ClientIsConnected.Should().BeTrue();
             _mocks.Reciever.Should().NotBeNull();
 
-            _mocks.SendAsync("rico" + Environment.NewLine, 0)
-                .ContinueWith(tresult =>
-                {
-                    var status = tresult.Status;
-                    status.Should().NotBeSameAs(TaskStatus.Faulted);
-                });
+            _mocks.SendAsync("rico" + Environment.NewLine, 0);
 
             var message = _mocks.Reciever.SubscribeOn(TaskPoolScheduler.Default)
                   .DistinctUntilChanged().Take(1).GetAwaiter().GetResult();
             message.Should().NotBeNull();
-            _output.WriteLine(message.Message.ToString());
+            message.EndPoint.ToString().Should().StartWith("127.0.0.1");
             message.Message.Should().Equals("rico");
-
+            message.Error.Should().BeNullOrEmpty();
+            _output.WriteLine(message.EndPoint.ToString());
+            _output.WriteLine(message.Message.ToString());
+            _output.WriteLine(message.Error);
             _output.WriteLine("Can_Send_Receive_Message done.");
         }
 
